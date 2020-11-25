6e6f2579070aa0ed6be07412bc088283b0b8d898
Ricardo Pereira <m@ricardopereira.eu>
2020-11-25 19:18:20 +0000

    SPM Package file

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "LogEntries",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "LogEntries",
+            targets: ["LogEntries"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "LogEntries",
+            path: ".",
+            sources: ["lelib"]
+        )
+    ]
+)
